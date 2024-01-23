e80a6e69a0856d0998bf0c134ce924db7faadf17
Ruvini-Rangathara <ruvinisubhasinghe200009@gmail.com>
2024-01-23 20:12:08 +0530

    updated

--- index.js
+++ index.js
@@ -1,52 +1,26 @@
-window.onload = function() {
-    var form1 = document.getElementById("burger-form");
-    var form2 = document.getElementById("beverage-form");
+$(document).ready(function() {
+    console.log('Document ready');
 
-    if (form1 && form2) {
-        form1.style.display = "none";
-        form2.style.display = "none";
-    }
-};
+    navigateTo('home');
+
+    $("#food").css('display', 'block');
+    $("#contact").css('display', 'block');
+
+    $('.nav-link').click(function() {
+        var sectionId = $(this).data('section-id');
+        navigateTo(sectionId);
+    });
+
+    $("#food-form, #customer-form").css('display', 'none');
+});
 
 function navigateTo(sectionId) {
     // Hide all sections
-    hideAllSections();
+    $('.content-section').css('display', 'none');
 
-    var selectedSection = document.getElementById(sectionId);
-
-    if (selectedSection) {
-        // Show the selected section
-        selectedSection.style.display = 'block';
-
-        // Show/hide forms based on the section
-        if (sectionId === 'food') {
-            var burgerForm = document.getElementById("burger-form");
-            var beverageForm = document.getElementById("beverage-form");
-
-            if (burgerForm && beverageForm) {
-                burgerForm.style.display = "block";
-                beverageForm.style.display = "block";
-            }
-        } else {
-            if (burgerForm && beverageForm) {
-                burgerForm.style.display = "none";
-                beverageForm.style.display = "none";
-            }
-        }
-
-    }
-
-
-
+    var selectedSection = $('#' + sectionId);
+    selectedSection.css('display', 'block');
 }
 
-// Hide all sections
-function hideAllSections() {
-    var sections = document.getElementsByClassName('content-section');
-    for (var i = 0; i < sections.length; i++) {
-        sections[i].style.display = 'none';
-    }
-}
 
-// Initial load of the first section
-navigateTo('home');
+///////////////////////////////////////////////// Customer Manage ////////////////////////////////////////////
